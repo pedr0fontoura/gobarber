5b6fe1b80645aac7ecdf7acb545c786c287c9355
Pedro Dias <pedrofontoura@id.uff.br>
2020-06-19 03:52:35 -0300

    feat: create Appointments model and repository

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,9 @@
 import { Router } from 'express';
 
+import appointmentsRouter from './appointments.routes';
+
 const routes = Router();
 
-routes.get('/', (request, response) =>
-  response.json({ message: 'Hello world!' }),
-);
+routes.use('/appointments', appointmentsRouter);
 
 export default routes;
--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,8 @@ import routes from './routes';
 
 const app = express();
 
+app.use(express.json());
+
 app.use(routes);
 
 app.listen(3333, () => {
