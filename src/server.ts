93e55ba88a9fc869a6347e7cbcd9a980793f50c2
Pedro Dias <pedrofontoura@id.uff.br>
2020-06-19 00:58:53 -0300

    Initial commit

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+
+import routes from './routes';
+
+const app = express();
+
+app.use(routes);
+
+app.listen(3333, () => {
+  console.log('Server started on :3333 !');
+});
