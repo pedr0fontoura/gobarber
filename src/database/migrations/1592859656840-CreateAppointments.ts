fd71d4d860cd1334eadb63b83957ea103b304a84
Pedro Dias <pedrofontoura@id.uff.br>
2020-06-23 21:11:18 -0300

    Relacionamento nos models

--- src/database/migrations/1592859656840-CreateAppointments.ts
+++ src/database/migrations/1592859656840-CreateAppointments.ts
@@ -9,7 +9,7 @@ export default class CreateAppointments1592859656840
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
