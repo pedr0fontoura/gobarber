b0c01440bbc6413ae3c77346f609f33d45c10dbb
Pedro Dias <pedrofontoura@id.uff.br>
2020-07-22 15:35:53 -0300

    Criando o primeiro teste

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,3 +1,23 @@
-test('sum two numbers', () => {
-  expect(1 + 2).toBe(3);
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
+import CreateAppointmentService from './CreateAppointmentService';
+
+describe('CreateAppointment', () => {
+  it('should be able to create a new appointment', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const createAppointmentService = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
+
+    const appointment = await createAppointmentService.execute({
+      date: new Date(),
+      provider_id: '12345678',
+    });
+
+    expect(appointment).toHaveProperty('id');
+    expect(appointment.provider_id).toBe('12345678');
+  });
+
+  /* it('should not be able to create two appointments on the same time', () => {
+    expect(1 + 2).toBe(3);
+  }); */
 });
