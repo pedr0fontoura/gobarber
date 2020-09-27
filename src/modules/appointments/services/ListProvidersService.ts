fd9f492369f4f767dca7cf3f64eb9e3ca35e7c45
Pedro Dias <pedrofontoura@id.uff.br>
2020-09-27 20:26:33 -0300

    Listagem de prestadores

---
+++ src/modules/appointments/services/ListProvidersService.ts
@@ -0,0 +1,27 @@
+import { injectable, inject } from 'tsyringe';
+
+import User from '@modules/users/infra/typeorm/entities/User';
+
+import IUsersRepository from '@modules/users/repositories/IUsersRepository';
+
+interface Request {
+  user_id: string;
+}
+
+@injectable()
+class ListProvidersService {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+  ) {}
+
+  public async execute({ user_id }: Request): Promise<User[]> {
+    const user = await this.usersRepository.findAllProviders({
+      except_user_id: user_id,
+    });
+
+    return user;
+  }
+}
+
+export default ListProvidersService;
--- src/modules/users/repositories/IUsersRepository.ts
+++ src/modules/users/repositories/IUsersRepository.ts
@@ -1,8 +1,10 @@
 import ICreateUserDTO from '@modules/users/dtos/ICreateUserDTO';
+import IFindAllProvidersDTO from '@modules/users/dtos/IFindAllProvidersDTO';
 
 import User from '../infra/typeorm/entities/User';
 
 interface IUsersRepository {
+  findAllProviders(data: IFindAllProvidersDTO): Promise<User[]>;
   findById(id: string): Promise<User | undefined>;
   findByEmail(email: string): Promise<User | undefined>;
   create(data: ICreateUserDTO): Promise<User>;
